97e72613fe3201d0738776323ba569ebf45a8d8e
TabattaC <Lavanda@1283>
2024-07-20 17:44:53 -0300

    Projeto spring boot: Adição de 'superclasse' abstrata

---
+++ src/main/java/com/projeto/curso/web/domain/AbstractEntity.java
@@ -0,0 +1,37 @@
+package com.projeto.curso.web.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity<?> that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + '}';
+    }
+}
