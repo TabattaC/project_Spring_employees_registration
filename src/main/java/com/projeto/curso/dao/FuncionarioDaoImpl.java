a9bf9c59d7d316308d68abc723fc797369578167
TabattaC <tabattacerri17@gmail.com>
2025-01-11 23:35:54 -0300

    add method findByDataEntrada, findByDataSaida, findByDataEntradaSaida

--- src/main/java/com/projeto/curso/dao/FuncionarioDaoImpl.java
+++ src/main/java/com/projeto/curso/dao/FuncionarioDaoImpl.java
@@ -3,6 +3,7 @@ package com.projeto.curso.dao;
 import com.projeto.curso.domain.Funcionario;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -15,5 +16,32 @@ public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implement
     @Override
     public List<Funcionario> findByCargoId(Long id) {
         return createQuery("select f from Funcionario f where f.cargo.id = ?1", id);
+    }
+
+    @Override
+    public List<Funcionario> findByDataEntrada(LocalDate entrada) {
+        String jpql = new StringBuilder("select f from Funcionario f ")
+                .append("where f.dataEntrada = ?1")
+                .append("order by f.dataEntrada asc")
+                .toString();
+        return createQuery(jpql,entrada);
+    }
+
+    @Override
+    public List<Funcionario> findByDataSaida(LocalDate saida) {
+        String jpql = new StringBuilder("select f from Funcionario f ")
+                .append("where f.dataSaida = ?1 ")
+                .append("order by f.dataEntrada asc")
+                .toString();
+        return createQuery(jpql,saida);
+    }
+
+    @Override
+    public List<Funcionario> findByDataEntradaSaida(LocalDate entrada, LocalDate saida) {
+       String jpql = new StringBuilder("select f from Funcionario f ")
+               .append("where f.dataEntrada >= ?1 and f.dataSaida <= ?2 ")
+               .append("order by f.dataEntrada asc")
+               .toString();
+       return createQuery(jpql,entrada,saida);
     }
 }
