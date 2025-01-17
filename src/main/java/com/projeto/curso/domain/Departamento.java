dff21b2d0bf60795e99d9df809fa83c8b5500e68
TabattaC <tabattacerri17@gmail.com>
2025-01-16 22:36:25 -0300

    add tags @NotNull, @PastOrPresent,  @NotNull

--- src/main/java/com/projeto/curso/domain/Departamento.java
+++ src/main/java/com/projeto/curso/domain/Departamento.java
@@ -5,13 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "DEPARTAMENTOS")
-@SuppressWarnings("serial")
 public class Departamento extends AbstractEntity<Long> {
-    @Column(name = "nome", nullable = false, unique = true, length = 60)
     @NotBlank(message = "Informe um nome.")
     @Size(min = 3, max = 60, message = "O nome do departamento deve ter entre {min} e {max} caracteres.")
+    @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
     @OneToMany(mappedBy = "departamento")
--- src/main/java/com/projeto/curso/domain/Funcionario.java
+++ src/main/java/com/projeto/curso/domain/Funcionario.java
@@ -1,6 +1,11 @@
 package com.projeto.curso.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -11,20 +16,32 @@ import java.time.LocalDate;
 @SuppressWarnings("serial")
 @Table(name = "FUNCIONARIOS")
 public class Funcionario extends AbstractEntity<Long> {
+    @NotBlank
+    @Size(max= 255, min = 3)
     @Column(nullable = false, unique = true)
     private String nome;
-    @Column(nullable = false, unique = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
+
+    @NotNull
     @NumberFormat(style = NumberFormat.Style.CURRENCY,pattern = "#,##0.00")
+    @Column(nullable = false, unique = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
     private BigDecimal salario;
+
+    @NotNull
+    @PastOrPresent(message = "{PastOrPresent.funcionario.dataEntrada}")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate dataEntrada;
-    @Column(name = "data_saida", columnDefinition = "DATE")
+
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name = "data_saida", columnDefinition = "DATE")
     private LocalDate dataSaida;
+
+    @Valid
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id_fk")
     private Endereco endereco;
+
+    @NotNull(message = "NotNull.funcionario.cargo")
     @ManyToOne
     @JoinColumn(name = "cargo_id_fk")
     private Cargo cargo;
